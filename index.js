3de272022833a27986d641afee2914ccee02f784
Marcus Wiseman <marcus.e.wiseman@gmail.com>
2019-04-19 01:42:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // add solution here
+var musicians = [];
+var instruments = [];
+
+function theBeatlesPlay(musicians, instruments) {
+  var emptyArray = [];
+  
+  for (let i = 0; i < musicians.length; i++) {
+    emptyArray.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return emptyArray;
+}
+
+function johnLennonFacts(facts) {
+  var newArray = [];
+  var count = 0;
+  
+  while (count < facts.length) {
+    newArray.push(`${facts[count]}` + "!!!");
+    count ++;
+  }
+  return newArray;
+}
